0b67b39ce20da7b1fa4dece5f0a304b0eaafbbb0
Alexander Stein <code+al@il5.in>
2016-03-30 23:41:36 +0300

    [org] Add ox-rss contrib to my files.
    As this component of org-mode is non-standard, add to my personal files,
    to periodically update directly from upstream SCM via the below URL.
    
    http://orgmode.org/cgit.cgi/org-mode.git/plain/contrib/lisp/ox-rss.el

--- personal/aurel.el
+++ personal/aurel.el
@@ -3,4 +3,4 @@
 (autoload 'aurel-package-search-by-name "aurel" nil t)
 (autoload 'aurel-maintainer-search "aurel" nil t)
 (autoload 'aurel-installed-packages "aurel" nil t)
-(setq aurel-download-directory "~/Code/makepkg/aurel")
+(setq aurel-download-directory "~/Code/makepkg/build")
