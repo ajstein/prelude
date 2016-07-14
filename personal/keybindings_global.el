a4907a4bc2631314f672a4ad0394a8fd123a64ac
Alexander Stein <xee5ch.gh.al@il5.in>
2016-07-15 01:02:04 +0300

    [meta] Clean up global keys in keybindings_global.

--- personal/keybindings_global.el
+++ personal/keybindings_global.el
@@ -1,3 +1,38 @@
+;;; keybindings_global.el --- My useful, but limted, keybindings.
+;;
+;; Copyright (c) 2016 Al S
+;;
+;; Author: Al S
+;; URL: https://github.com/xee5ch/prelude.git
+;; Version: 0.0.1
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Lots of useful keybindings.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
 (global-set-key (kbd "M-[") 'previous-buffer)
 
 (global-set-key (kbd "M-]") 'next-buffer)
+;;; keybindings_global.el ends here
