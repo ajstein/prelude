00dee5841fec8631b8ee66b752c5fb7320202686
Alexander Stein <xee5ch.gh.al@il5.in>
2016-07-15 00:57:56 +0300

    [term] Fix C-a binding to prelude & crux.
    The prelude package collection by default uses crux as a util library of
    useful minor mode functions with some convenient and inconvenient
    functions.
    
    http://stackoverflow.com/q/38266293
    https://github.com/bbatsov/prelude/issues/998
    https://github.com/bbatsov/prelude/issues/1009

--- personal/multi-term.el
+++ personal/multi-term.el
@@ -1,3 +1,36 @@
+;;; multi-term.el --- My useful, but limted, keybindings.
+;;
+;; Copyright (c) 2016 Al S
+;;
+;; Author: Al S
+;; URL: https://github.com/xee5ch/prelude.git
+;; Version: 0.0.1
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Customizations for term and multi-term usage.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
 (require 'multi-term)
 
 (defun term-send-C-x ()
@@ -35,3 +68,17 @@
                             ("M-r"           . term-send-reverse-search-history)
                             ("M-,"           . term-send-input)
                             ("M-."           . comint-dynamic-complete)))
+
+;;; Useful recommended code from:
+;;; http://emacsredux.com/blog/2013/09/25/removing-key-bindings-from-minor-mode-keymaps/
+(defun my-term-mode-hook ()
+  (let ((oldmap (cdr (assoc 'prelude-mode minor-mode-map-alist)))
+        (newmap (make-sparse-keymap)))
+    (set-keymap-parent newmap oldmap)
+    (define-key newmap (kbd "C-a") nil)
+    (make-local-variable 'minor-mode-overriding-map-alist)
+    (push `(prelude-mode . ,newmap) minor-mode-overriding-map-alist))
+  )
+
+(add-hook 'term-mode-hook 'my-term-mode-hook)
+;;; multi-term.el ends here
